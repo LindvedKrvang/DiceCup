ff5296441b7bb6a79f4ffd6541aa8caadb96e956
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-02-26 15:22:17 +0100

    Able to create between 1 and 6 dice

--- app/src/main/java/com/krvang/lindved/dicecup/RollActivity.java
+++ app/src/main/java/com/krvang/lindved/dicecup/RollActivity.java
@@ -1,14 +1,23 @@
 package com.krvang.lindved.dicecup;
 
+import android.content.Context;
+import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 public class RollActivity extends AppCompatActivity {
+
+    private final int ROW_DIVIDER = 3;
 
     private Button mRollButton;
     private ImageButton mHistoryButton;
@@ -20,12 +29,13 @@ public class RollActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll);
 
-        initializeLayout();
         initializeSpinner();
+        initializeLayout();
     }
 
     private void initializeLayout(){
         mDiceLayout = findViewById(R.id.diceLayout);
+        createDiceBoard(Integer.parseInt(mNumbersSpinner.getSelectedItem().toString()));
     }
 
     private void initializeSpinner(){
@@ -35,5 +45,49 @@ public class RollActivity extends AppCompatActivity {
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mNumbersSpinner.setAdapter(adapter);
+        mNumbersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //TODO RKL: Show dice according to selected number.
+                int dice = Integer.parseInt(parent.getItemAtPosition(position).toString());
+                createDiceBoard(dice);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void createDiceBoard(int amount){
+        mDiceLayout.removeAllViews();
+        LinearLayout firstRow = new LinearLayout(this);
+        for (int i = 0; i < ROW_DIVIDER; i++){
+            if(i < amount){
+                createSingleDice(firstRow);
+            }
+        }
+        mDiceLayout.addView(firstRow);
+        if(amount > ROW_DIVIDER){
+            LinearLayout secondRow = new LinearLayout(this);
+            for(int i = ROW_DIVIDER; i < amount; i++){
+                createSingleDice(secondRow);
+            }
+            mDiceLayout.addView(secondRow);
+        }
+    }
+
+    private void createSingleDice(LinearLayout layout){
+        Dice dice = new Dice(this);
+        layout.addView(dice);
+    }
+
+    class Dice extends AppCompatImageView {
+
+        public Dice(Context context) {
+            super(context);
+            setImageResource(R.drawable.dice_one);
+        }
     }
 }
